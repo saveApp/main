a18408a37d9995b97edf2e4d529672dec836d5cb
Risdani Agung <eater.luck@gmail.com>
2016-09-08 16:00:42 +1000

    bismillah

---
+++ PassingData/Result.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PassingData
+{
+    public partial class Result : ContentPage
+    {
+        public Result()
+        {
+            InitializeComponent();
+        }
+    }
+}
